f03e4394b272bcfbb56e750a17965f5663de4682
Martin Vassor <martin.vassor@epfl.ch>
2016-01-16 11:30:36 +0100

    Allocation/Free

--- src/sstm_alloc.c
+++ src/sstm_alloc.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sched.h>
+#include <string.h>
 
 __thread sstm_alloc_t sstm_allocator = { .n_allocs = 0 };
-__thread sstm_alloc_t sstm_freeing = { .n_allocs = 0 };
+__thread sstm_alloc_t sstm_freeing = { .n_frees = 0 };
 
 /* allocate some memory within a transaction
  */
@@ -13,7 +14,8 @@ sstm_tx_alloc(size_t size)
 {
 	assert(sstm_allocator.n_allocs < SSTM_ALLOC_MAX_ALLOCS);
 	void* m = malloc(size);
-
+	sstm_allocator.mem[sstm_allocator.n_allocs] = m;
+	sstm_allocator.n_allocs++;
 	/* 
 	   you need to keep track of allocations, so that if a TX
 	   aborts, you free that memory
@@ -28,7 +30,8 @@ sstm_tx_alloc(size_t size)
 sstm_tx_free(void* mem)
 {
 	assert(sstm_freeing.n_allocs < SSTM_ALLOC_MAX_ALLOCS);
-
+	sstm_freeing.mem[sstm_freeing.n_frees] = mem;
+	sstm_freeing.n_frees++;
 	/* 
 	   in a more complex STM systems,
 	   you cannot immediately free(mem) as below because you might 
@@ -36,7 +39,7 @@ sstm_tx_free(void* mem)
 	   and only make the actual free happen if the TX is commited
 	 */
 
-	free(mem);
+	/*free(mem);*/
 }
 
 /* this function is executed when a transaction is aborted.
@@ -47,7 +50,13 @@ sstm_tx_free(void* mem)
 	void
 sstm_alloc_on_abort()
 {
-
+	size_t i;
+	for(i = 0 ; i < sstm_allocator.n_allocs ; i++)
+	{
+		free(sstm_allocator.mem[i]);
+	}
+	sstm_allocator.n_allocs = 0;
+	sstm_freeing.n_frees = 0;
 }
 
 /* this function is executed when a transaction is committed.
@@ -59,4 +68,11 @@ sstm_alloc_on_abort()
 sstm_alloc_on_commit()
 {
 
+	size_t i;
+	for(i = 0 ; i < sstm_freeing.n_frees ; i++)
+	{
+		free(sstm_freeing.mem[i]);
+	}
+	sstm_allocator.n_allocs = 0;
+	sstm_freeing.n_frees = 0;
 }
